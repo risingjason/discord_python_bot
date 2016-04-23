349383af857672c3cf4b9bcbe7ed364626862773
risingjason <jzhang5137@gmail.com>
2016-04-23 03:28:14 -0700

    Updated bot.py to v1.1
    Updated bot.py to v1.1

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ fp_infos.close()
 
 client = discord.Client()
 
-#online_message = "```Chinatown Discord Bot is now online!\n" +\
-#				 "Type !help for a list of commands.```"
+online_message = "```Chinatown Discord Bot v1.1 is now online!\n" +\
+				 "Type !help for a list of commands.```"
 
 
 fp_kappa = open("kappaLibrary.txt", "r")
@@ -28,13 +28,14 @@ async def on_ready():
 	for channel in client.get_all_channels():
 		if channel.type is discord.ChannelType.text: #notifies all registered text channels that the bot is online
 			print("Bot is running on server: {}, name: {}, id: {}, type: {}".format(channel.server, channel.name.encode("utf-8"), channel.id, channel.type))
-			await client.send_message(channel, online_message)
+			#await client.send_message(channel, online_message)
 
 @client.event
 async def on_message(msg):
 	cmds = msg.content.split(' ') # seperates the message word by word
 	cmd = cmds[0].lower() # takes the first word (most commands are called using the first word of message)
 	mentions = msg.raw_mentions
+
 
 	if cmd in core.commands:
 		await core.commands[cmd](client, msg, cmds)
